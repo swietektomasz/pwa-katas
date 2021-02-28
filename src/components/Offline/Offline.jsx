803fbd0c35792e376aff2e830569ad58c965a81c
swietektomasz <swietek.tomasz@protonmail.com>
2021-02-28 14:13:27 +0100

    Offile notes v1

--- src/components/Offline/Offline.jsx
+++ src/components/Offline/Offline.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export function Offline() {
-  return <div>Offline</div>;
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div>
+      <input type="text" value={title} name="title" onChange={(event) => setTitle(event.target.value)} />
+      <input
+        type="text"
+        value={description}
+        name="description"
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button onClick={() => setNotes((prevNotes) => [...prevNotes, { title, description }])}>Add note</button>
+      {notes.map(({ title, description }) => (
+        <div key={`${title}-${description}`}>
+          <h2>{title}</h2>
+          <span>{description}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
